d8268ac529837989dfe4f2177546184ac472b054
Caleb <cmennen@vols.utk.edu>
2016-10-30 12:43:58 -0400

    remove numpy dependancy

--- hough_p.py
+++ hough_p.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 
 video = cv2.VideoCapture("videos/top_down_4" + ".m4v")
 
@@ -11,19 +10,19 @@ while True:
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # These are calculated by hand, super iffy on if they will break or not
-    color_min = np.array([30, 0, 240])
-    color_max = np.array([80, 70, 255])
+    color_min = (30, 0, 240)
+    color_max = (80, 70, 255)
 
     mask = cv2.inRange(hsv, color_min, color_max)
     edges = cv2.Canny(mask, 50, 200, apertureSize=3)
 
-    lines = cv2.HoughLinesP(mask, 1, np.pi / 180, 100, minLineLength=100, maxLineGap=10)
+    lines = cv2.HoughLinesP(mask, 1, 3.141592653589793 / 180, 100, minLineLength=100, maxLineGap=10)
 
     if lines is None:
         continue
     for x1, y1, x2, y2 in lines[0]:
         cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 5)
-        cv2.line(frame, (0, 0), (100, 100), (0, 0, 255), 5)
+        # cv2.line(frame, (0, 0), (100, 100), (0, 0, 255), 5)
 
     cv2.imshow('linesH', frame)
 
